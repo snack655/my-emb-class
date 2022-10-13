26b99217ba1549a5815836a0c9a48661dcbaac2a
snack655 <80940200+snack655@users.noreply.github.com>
2022-10-13 10:51:32 +0900

    Tetris UI develop

---
+++ C#/secondTetris/tetris/tetris/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace tetris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(447, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(220, 442);
+            this.panel1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 677);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ C#/secondTetris/tetris/tetris/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tetris
+{
+    public partial class Form1 : Form
+    {
+        int x = 0;
+        int y = 0;
+
+        byte[,] block_L = new byte[4, 4]
+        {
+            { 0, 0, 0, 0 },
+            { 0, 1, 0, 0 },
+            { 0, 1, 1, 1 },
+            { 0, 0, 0, 0 }
+        };
+
+        byte[,] background = new byte[22, 12]
+        {
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            make_block();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (overlap_check(0, 1) == 0)
+            {
+                delete_block();
+                y++;
+                make_block();
+            }
+
+        }
+
+        void make_block()
+        {
+
+            Graphics g = this.panel1.CreateGraphics();
+            Pen p = new Pen(Color.Red, 2);
+            Rectangle rec;
+
+
+
+            for (int j = 0; j < 4; j++)
+            {
+                for(int i = 0; i < 4; i++)
+                {
+                    if (block_L[j, i] == 1)
+                    {
+                        rec = new Rectangle( (0 + 3) + (x * 22) + (i * 22), (0 + 3) + (y * 22) + (j * 22), 18, 18);
+                        g.DrawRectangle(p, rec);
+
+                    }
+                }
+            }
+
+            p.Dispose();
+            g.Dispose();
+        }
+
+        void delete_block()
+        {
+
+            Graphics g = this.panel1.CreateGraphics();
+            Pen p = new Pen(Color.White, 2);
+            Rectangle rec;
+
+
+
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (block_L[j, i] == 1)
+                    {
+                        rec = new Rectangle((0 + 3) + (x * 22) + (i * 22), (0 + 3) + (y * 22) + (j * 22), 18, 18);
+                        g.DrawRectangle(p, rec);
+
+                    }
+                }
+            }
+
+            p.Dispose();
+            g.Dispose();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar =='a')
+            {
+                if (overlap_check(-1, 0) == 0)
+                {
+                    delete_block();
+                    x--;
+                    make_block();
+                }
+            } else if (e.KeyChar == 'd')
+            {
+                if (overlap_check(1, 0) == 0)
+                {
+                    delete_block();
+                    x++;
+                    make_block();
+                }
+            } else if (e.KeyChar == 's')
+            {
+                if (overlap_check(0, 1) == 0)
+                {
+                    delete_block();
+                    y++;
+                    make_block();
+                }
+            }
+        }
+
+        int overlap_check(int offset_x, int offset_y)
+        {
+            int overlap_count = 0;
+
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (block_L[j, i] == 1 && background[j + y + 1 + offset_y, i + x + 1 + offset_x] == 1)
+                    {
+                        overlap_count++;
+
+                    }
+                }
+            }
+            return overlap_count;
+        }
+    }
+}
