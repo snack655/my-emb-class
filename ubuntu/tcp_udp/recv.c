b9c64bc90df4f317459ffddd76cf558110c5274c
snack655 <80940200+snack655@users.noreply.github.com>
2022-10-20 11:00:38 +0900

    Study : Tcp, Udp linux....

---
+++ ubuntu/tcp_udp/recv.c
@@ -0,0 +1,73 @@
+
+#include<stdio.h>
+#include<malloc.h>
+#include<string.h>
+#include<signal.h>
+#include<stdbool.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+
+#include<linux/if_packet.h>
+#include<netinet/in.h>		 
+#include<netinet/if_ether.h>    // for ethernet header
+#include<netinet/ip.h>		// for ip header
+#include<netinet/udp.h>		// for udp header
+#include<netinet/tcp.h>
+#include<arpa/inet.h>           // to avoid warning at inet_ntoa
+
+struct sockaddr saddr;
+struct sockaddr_in source,dest;
+
+void recv_data_process(unsigned char* buffer,int buflen);
+
+int main()
+{
+
+	int sock_recv,saddr_len,buflen;
+
+	unsigned char* buffer = (unsigned char *)malloc(65536); 
+	memset(buffer,0,65536);
+
+	printf("starting .... \n");
+
+	sock_recv=socket(AF_PACKET,SOCK_RAW,htons(ETH_P_ALL)); 
+	if(sock_recv<0)
+	{
+		printf("error in socket\n");
+		return -1;
+	}
+
+	while(1)
+	{
+		saddr_len=sizeof saddr;
+		buflen=recvfrom(sock_recv,buffer,65536,0,&saddr,(socklen_t *)&saddr_len);
+
+		if(buflen<0)
+		{
+			printf("error in reading recvfrom function\n");
+			return -1;
+		}
+
+		//---------------------------------------
+		recv_data_process(buffer,buflen);
+		//---------------------------------------
+	}
+
+	close(sock_recv);// use signals to close socket 
+	printf("DONE!!!!\n");
+}
+
+
+//================================================================
+
+
+void recv_data_process(unsigned char* buffer,int buflen)
+{
+
+	if(buffer[1] == 0x01 && buffer[2] == 0x02)
+	{
+		for(int i=0;i<14;i++) printf("%02x ", buffer[i]);
+		printf("\n");			
+	}
+
+}
