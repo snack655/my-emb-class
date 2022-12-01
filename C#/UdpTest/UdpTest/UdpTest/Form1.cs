0ae7de68d894871cfd30f5be9b25bf7054fa15e8
snack655 <80940200+snack655@users.noreply.github.com>
2022-12-01 11:05:19 +0900

    Add : Form

---
+++ C#/UdpTest/UdpTest/UdpTest/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UdpTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] data = new byte[1024];
+
+            UdpClient client = new UdpClient("192.168.71.129", 10018);
+
+            data = Encoding.UTF8.GetBytes("kiki");
+            client.Send(data, data.Length);
+
+            client.Close();
+            Console.WriteLine("Stopping clinet");
+        }
+    }
+}
