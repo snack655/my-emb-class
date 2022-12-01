46b58376936d48931156025c57bdf5a5d12ebb7d
snack655 <80940200+snack655@users.noreply.github.com>
2022-12-01 13:27:31 +0900

    Update : Window form

--- C#/UdpTest/UdpTest/UdpTest/Form1.cs
+++ C#/UdpTest/UdpTest/UdpTest/Form1.cs
@@ -23,12 +23,77 @@ namespace UdpTest
         {
             byte[] data = new byte[1024];
 
-            UdpClient client = new UdpClient("192.168.71.129", 10018);
+            UdpClient client = new UdpClient("192.168.137.235", 10018);
 
-            data = Encoding.UTF8.GetBytes("kiki");
+            data = Encoding.UTF8.GetBytes("led1on");
             client.Send(data, data.Length);
 
             client.Close();
+            Console.WriteLine("Stopping clinet");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] data = new byte[1024];
+
+            UdpClient client = new UdpClient("192.168.137.235", 10018);
+
+            data = Encoding.UTF8.GetBytes("led1of");
+            client.Send(data, data.Length);
+
+            client.Close();
+            Console.WriteLine("Stopping clinet");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            byte[] data = new byte[1024];
+
+            UdpClient client = new UdpClient("192.168.137.235", 10018);
+
+            data = Encoding.UTF8.GetBytes("led2on");
+            client.Send(data, data.Length);
+
+            client.Close();
+            Console.WriteLine("Stopping clinet");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            byte[] data = new byte[1024];
+
+            UdpClient client = new UdpClient("192.168.137.235", 10018);
+
+            data = Encoding.UTF8.GetBytes("led2of");
+            client.Send(data, data.Length);
+
+            client.Close();
+            Console.WriteLine("Stopping clinet");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            byte[] data = new byte[1024];
+
+            UdpClient client = new UdpClient("192.168.137.235", 10018);
+
+            data = Encoding.UTF8.GetBytes("serlef");
+            client.Send(data, data.Length);
+
+            client.Close();
+            Console.WriteLine("Stopping clinet");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            byte[] data = new byte[1024];
+
+            UdpClient client = new UdpClient("192.168.137.235", 10018);
+
+            data = Encoding.UTF8.GetBytes("serrit");
+            client.Send(data, data.Length);
+
+            client.Close(); 
             Console.WriteLine("Stopping clinet");
         }
     }
