38d06025be88657a5ca0a2a19332e6d5342220c4
snack655 <cmjcmj@gmail.com>
2022-12-01 12:34:26 +0900

    3차 수행

--- secondTest/serial/serial.ino
+++ secondTest/serial/serial.ino
@@ -1,13 +1,47 @@
+#define LED_PIN 13
+#define LED_PIN2 6
+#define SERVO_PIN 5
+
+int cycle = 1000;
+
 void setup() {
+  Serial.setTimeout(100);
   Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_PIN2, OUTPUT);
+  pinMode(SERVO_PIN, OUTPUT);
 }
 
 void loop() {
 
   if (Serial.available() > 0) {
-    String data = Serial.readStringUntil('\n');
+    String str = Serial.readStringUntil('\n');
+    Serial.println(str);
 
-    Serial.println(data);
+    if(str == "led1on") {
+      digitalWrite(LED_PIN, HIGH);
+    } else if(str == "led1of") {
+      digitalWrite(LED_PIN, LOW);
+    } else if(str == "led2on") {
+      digitalWrite(LED_PIN2, HIGH);
+    } else if(str == "led2of") {
+      digitalWrite(LED_PIN2, LOW);
+    } else if(str == "serlef") {
+      servo(SERVO_PIN, 2400);
+    } else if(str == "serrit") {
+      servo(SERVO_PIN, 500);
+    }
   }
 
+}
+
+int interval = 20000;
+
+void servo(int pin, int cycle) {
+  for (int i = 0; i < 100; i++) {
+    digitalWrite(pin, HIGH);
+    delayMicroseconds(cycle);
+    digitalWrite(pin, LOW);
+    delayMicroseconds(interval - cycle);
+  }
 }
