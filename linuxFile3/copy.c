f6d34dca3b08d3cc2b292d995543691d570c7546
snack655 <cmjcmj1230@gmail.com>
2023-04-04 11:01:44 +0900

    add file copy

---
+++ linuxFile3/copy.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	if (argc != 3)
+	{
+		printf("사용법\n");
+		printf("copy file [source filename] [out filename]\n");
+		return 0;
+	}
+
+	// 1. line 으로 읽어와서 변수에 저장
+	// 2. 읽은 거 새로운 파일에 쓰기
+	
+	FILE* fileRead = fopen(argv[1], "rb");
+	FILE* fileWrite = fopen(argv[2], "wb");
+
+	if (fileRead == NULL || fileWrite == NULL) return 0;
+
+	char file;
+
+	while(!feof(fileRead))
+	{
+		fread(&file, sizeof(char), 1, fileRead);
+		fwrite(&file, sizeof(char), 1, fileWrite);
+	}
+
+	fclose(fileRead);
+	fclose(fileWrite);
+
+	return 0;
+}
---
+++ linuxFile3/fcopy.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+#define BUFFER_SIZE 1024
+
+int main(int argc, char* argv[]) 
+{
+	if (argc != 3) {
+		printf("응애 그렇게 쓰는거 아닌뒙");
+		return 0;
+	}
+
+	FILE* fread = open(argv[1], O_RDONLY);
+	FILE* fwrite = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	
+	if(fread == -1 && fwrite == -1) return 0;
+	
+	char buffer[BUFFER_SIZE]; 
+	int bytes_read;
+
+	while((bytes_read = read(fread, buffer, BUFFER_SIZE)) > 0) {
+		write(fwrite, buffer, bytes_read);
+	}	
+
+	close(fread);
+	close(fwrite);
+
+	printf("다했다 임마"); 
+
+	return 0;
+}
